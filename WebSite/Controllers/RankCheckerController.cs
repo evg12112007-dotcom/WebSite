2869cdbb947f9a61a2f995c4b4b7b84558b8295e
Nekk01 <54298798+Nekk01@users.noreply.github.com>
2025-11-03 03:33:19 +0800

    Добавлена интеграция с API leetify, для будущей системы проверки ранга (сейчас только вывод статистик пользователей)

---
+++ WebSite/Controllers/RankCheckerController.cs
@@ -0,0 +1,60 @@
+﻿using WebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WebSite.Controllers
+{
+    public class RankCheckerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Check(RankCheckerViewModel InputSteamID)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index");
+            }
+
+            string SteamID = InputSteamID.SteamID;
+            string URL = $"https://api-public.cs-prod.leetify.com/v3/profile?steam64_id={SteamID}";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+                    HttpResponseMessage response = await client.GetAsync(URL);
+                    response.EnsureSuccessStatusCode();
+
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(jsonResponse);
+
+
+                    //Пока вывожу рейтинг, потом можно поменять на другие статы (сейчас не работает, потом починю)
+                    var playerStats = new
+                    {
+                        Aim = data.rating.aim,
+                        Positioning = data.rating.positioning,
+                        Utility = data.rating.utility,
+                        Clutch = data.rating.clutch,
+                        Opening = data.rating.opening,
+                        CTLeetify = data.rating.ct_leetify,
+                        TLeetify = data.rating.t_leetify
+                    };
+
+                    return Content(jsonResponse, "application/json");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+                return StatusCode(500, "Произошла ошибка при обработке запроса.");
+            }
+        }
+    }
+}
