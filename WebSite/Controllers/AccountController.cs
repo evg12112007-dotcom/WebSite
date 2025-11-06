e48370887746fed35672d285e150fe938e734653
LolkoXb <LolkoXb@yandex.ru>
2025-11-06 22:53:41 +0800

    Добавлены страницы: вход, регистрация, сброс пароля (стартовая), сброс пароля (смена) + ссылки на них. Добавлена картинка для страниц, связанных с входом в аккаунт. Слегка оптимизирован код в "site.css". Переименован файл "sipmle.png" -> "simple.png". :P

---
+++ WebSite/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebSite.Models;
+
+namespace WebSite.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult AccountCreation()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult PasswordRecovery()
+        {
+            return View();
+        }
+        public IActionResult PasswordRecoveryReset()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebSite/Views/Account/PasswordRecovery.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebSite.Views.Account
+{
+    public class Password_recoveryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
