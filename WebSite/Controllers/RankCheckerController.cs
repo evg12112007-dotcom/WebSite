220f3292adefdb64ee09800b9b40ede0a5ccc6b5
Nekk01 <54298798+Nekk01@users.noreply.github.com>
2025-11-09 01:47:26 +0800

    Исправлены ошибки в коде

--- WebSite/Controllers/RankCheckerController.cs
+++ WebSite/Controllers/RankCheckerController.cs
@@ -23,12 +23,13 @@ namespace WebSite.Controllers
             }
 
             string SteamID64 = InputSteamID.SteamID;
-            var SteamID32 = long.Parse(SteamID64) - 76561197960265728;
+            string SteamID32 = CalculateSteamID32(SteamID64);
             string LeetifyURL = $"https://api-public.cs-prod.leetify.com/v3/profile?steam64_id={SteamID64}";
             string OpenDotaURL = $"https://api.opendota.com/api/players/{SteamID32}";
             int CS2SkillLevel = 0;
             int Dota2SkillLevel = 0;
 
+            // Запрос к Leetify
             try
             {
                 using (HttpClient client = new HttpClient())
@@ -42,19 +43,11 @@ namespace WebSite.Controllers
                     using JsonDocument CS2document = JsonDocument.Parse(CS2JsonResponse);
                     JsonElement CS2root = CS2document.RootElement;
 
-                    // Запрос к OpenDota API
-                    HttpResponseMessage OpenDotaResponse = await client.GetAsync(OpenDotaURL);
-                    OpenDotaResponse.EnsureSuccessStatusCode();
-                    string OpenDotaJsonResponse = await OpenDotaResponse.Content.ReadAsStringAsync();
-                    using JsonDocument OpenDotadocument = JsonDocument.Parse(OpenDotaJsonResponse);
-                    JsonElement OpenDotaRoot = OpenDotadocument.RootElement;
-
                     // В строку какие категории будем вытаскивать из JSON'а:
                     JsonElement CS2Rating = CS2root.GetProperty("ranks");
-                    JsonElement Dota2Rating = OpenDotaRoot.GetProperty("profile");
 
                     // Здесь прописываются критерии отбора (CS2)
-                    switch (CS2Rating.GetProperty("faceit").GetDouble()) 
+                    switch (CS2Rating.GetProperty("faceit").GetDouble())
                     {
                         case 0: // Игрок не может участвовать
                             CS2SkillLevel = 0;
@@ -76,53 +69,75 @@ namespace WebSite.Controllers
                             CS2SkillLevel = 4;
                             break;
                     }
-
-                    // Здесь прописываются критерии отбора (Dota2)
-                    switch (Dota2Rating.GetProperty("rank_tier").GetDouble() / 100)
-                    {
-                        case 0: // Игрок не может участвовать
-                            Dota2SkillLevel = 0;
-                            break;
-
-                        case > 0 and <= 2.5: // Слабый игрок
-                            Dota2SkillLevel = 1;
-                            break;
-
-                        case > 2.5 and <= 5: // Средний игрок
-                            Dota2SkillLevel = 2;
-                            break;
-
-                        case > 5 and <= 7.5: // Хороший игрок
-                            Dota2SkillLevel = 3;
-                            break;
-
-                        case > 7.5: // Отличный игрок
-                            Dota2SkillLevel = 4;
-                            break;
-                    }
-
-                    // Объект со значениями JSON (которые можем записывать в бд, или еще куда-то)
-                    var playerStats = new
-                    {
-                        leetify = CS2Rating.GetProperty("leetify").GetDouble(),
-                        premier = CS2Rating.GetProperty("premier").GetDouble(),
-                        faceit = CS2Rating.GetProperty("faceit").GetDouble(),
-                        faceit_elo = CS2Rating.GetProperty("faceit_elo").GetDouble(),
-                        wingman = CS2Rating.GetProperty("wingman").GetDouble(),
-                        personaname = Dota2Rating.GetProperty("personaname").GetString(),
-                        rank_tier = OpenDotaRoot.GetProperty("rank_tier").GetDouble(), // Если у пользователя нет ранга (по каким то причинам), может выдать исключение
-                        CS2_Tourniment_Access_Level = CS2SkillLevel,  // Может ли юзер участвовать в турнире?
-                        Dota2_Tourniment_Access_Level = Dota2Rating  // Может ли юзер участвовать в турнире?
-                    };
-
-                    return Json(playerStats);
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Ошибка: {ex.Message}");
-                return StatusCode(500, "Произошла ошибка при обработке запроса.");
             }
+
+            // Запрос к OpenDota
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+                    // Запрос к OpenDota API
+                    HttpResponseMessage OpenDotaResponse = await client.GetAsync(OpenDotaURL);
+                    OpenDotaResponse.EnsureSuccessStatusCode();
+                    string OpenDotaJsonResponse = await OpenDotaResponse.Content.ReadAsStringAsync();
+                    using JsonDocument OpenDotadocument = JsonDocument.Parse(OpenDotaJsonResponse);
+                    JsonElement OpenDotaRoot = OpenDotadocument.RootElement;
+
+                    // В строку какие категории будем вытаскивать из JSON'а:
+                    //JsonElement Dota2Rating = OpenDotaRoot.GetProperty("profile");
+
+                    // Здесь прописываются критерии отбора (Dota2)
+                    switch (OpenDotaRoot.GetProperty("rank_tier").GetDouble())
+                    {
+                        case 0: // Игрок не может участвовать
+                            Dota2SkillLevel = 0;
+                            break;
+
+                        case > 0 and <= 21: // Слабый игрок
+                            Dota2SkillLevel = 1;
+                            break;
+
+                        case > 21 and <= 43: // Средний игрок
+                            Dota2SkillLevel = 2;
+                            break;
+
+                        case > 43 and <= 65: // Хороший игрок
+                            Dota2SkillLevel = 3;
+                            break;
+
+                        case > 65: // Отличный игрок
+                            Dota2SkillLevel = 4;
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+
+            // Ответ
+            var result = new
+            {
+                CS2SkillLevel = CS2SkillLevel,
+                Dota2SkillLevel = Dota2SkillLevel
+            };
+            return Json(result);
         }
+
+        public static string CalculateSteamID32(string steamID64)
+        {
+            long steamID64Long = long.Parse(steamID64);
+            long steamID32Long = steamID64Long - 76561197960265728;
+            return steamID32Long.ToString();
+        }
+
     }
 }
